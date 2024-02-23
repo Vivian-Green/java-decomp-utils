93bf4df0d4b483c1334575ec9154e5667624e18f
Vivian Corbett <acorbett0528@gmail.com>
2024-02-22 22:17:14 -0800

    tweaks, strip to interface now outputs to file

--- StripJavaToInterface.py
+++ StripJavaToInterface.py
@@ -55,54 +55,66 @@ def is_end_of_java_declaration(line):
         return stripped_line.index("{")
     return -1
 
-def print_java_declarations(file_path):
+def print_java_declarations(path):
     print("\n\n")
     try:
-        with open(file_path, 'r') as file:
+        with open(path, 'r') as file:
             content = file.read()
-            lines = remove_comments_and_strings_from_content(content)
-
-            inside_declaration = False
-            declaration = "\n"
-
-            nesting = 0
-
-            classEntryNesting = -1
-
-            for line in lines:
-                if "class" in line.lower():
-                    classEntryNesting = nesting
-                    if "{" in line:
-                        print(line[:line.index("{")+1])
-
-                # print(line)
-                if is_start_of_java_declaration(line):
-                    inside_declaration = True
-                    declaration = line.strip()
-                if inside_declaration:
-                    declaration += line.strip()
-                    end_index = is_end_of_java_declaration(line)
-                    if end_index != -1:
-                        declaration = declaration[:end_index + 1]
-                        print("    " * nesting + declaration + "}")
-                        inside_declaration = False
-                        declaration = ""
-
-                nesting += line.count('{') - line.count('}')
-
-                #print(" "*nesting + str(nesting))
-
-                if classEntryNesting > nesting and "}" in line:
-                    classEntryNesting = -1
-                    print("}")
-
-
-
-            print("\n\n")
     except FileNotFoundError:
-        print(f"File not found: {file_path}")
+        print(f"File not found: {path}")
+        return
     except Exception as e:
         print(f"An error occurred: {e}")
+        return
+
+    lines = remove_comments_and_strings_from_content(content)
+
+    inside_declaration = False
+    declaration = "\n"
+
+    nesting = 0
+
+    classEntryNesting = -1
+
+    newLines = []
+
+    for line in lines:
+        if "class" in line.lower():
+            classEntryNesting = nesting
+            if "{" in line:
+                thisSignature = line[:line.index("{") + 1]
+                print(thisSignature)
+                newLines.append(thisSignature + "\n")
+
+        # print(line)
+        if is_start_of_java_declaration(line):
+            inside_declaration = True
+            declaration = line.strip()
+        if inside_declaration:
+            declaration += line.strip()
+            end_index = is_end_of_java_declaration(line)
+            if end_index != -1:
+                declaration = declaration[:end_index + 1]
+                thisSignature = "    " * nesting + declaration + "}"
+                print(thisSignature)
+                newLines.append(thisSignature + "\n")
+                inside_declaration = False
+                declaration = ""
+
+        nesting += line.count('{') - line.count('}')
+
+        # print(" "*nesting + str(nesting))
+
+        if classEntryNesting > nesting and "}" in line:
+            classEntryNesting = -1
+            print("}")
+            newLines.append("}")
+    
+
+    with open(path.replace('.java', '.interface.txt'), 'w') as file:
+        file.writelines(newLines)
+
+    print("\n\n")
 
 
 if __name__ == "__main__":
